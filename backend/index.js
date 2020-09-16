20a459a5e1aad201ff9c1729e22ca34ce7e12bd5
rdeepak2002 <rdeepak2002@gmail.com>
2020-09-15 21:35:12 -0700

    added sample react app

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+app.listen(8080);
