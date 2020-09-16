3b99b62afc20ba87449707e0cc71b88d51e7d1fc
rdeepak2002 <rdeepak2002@gmail.com>
2020-09-15 21:44:22 -0700

    fixed port bindings

--- backend/index.js
+++ backend/index.js
@@ -7,4 +7,4 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
