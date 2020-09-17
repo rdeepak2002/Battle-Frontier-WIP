4dd9b9bd94d07f5310064952032f1d6cbc32be81
rdeepak2002 <rdeepak2002@gmail.com>
2020-09-16 21:41:07 -0700

    disabled mobilie zooming

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// defining the static directory as the build folder of the frontend
 app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+/**
+ * function to return the frontend when the root url is visited
+ */
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-
 // TODO: look into this - https://www.codevate.com/blog/developing-a-scalable-real-time-desktop-or-mobile-application-with-socketio-redis-and-haproxy
 
-
+// application listening on the default port or 5000
 app.listen(process.env.PORT || 5000);
